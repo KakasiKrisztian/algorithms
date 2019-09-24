8e539349c4e7067fac726e93c10292533c2225d9
Krisztian Kakasi <cristy11_1998@yahoo.com>
2019-09-24 12:58:35 +0300

    for loop homework added / updated

--- src/main/java/org/fasttrackit/LogicalOp.java
+++ src/main/java/org/fasttrackit/LogicalOp.java
@@ -39,49 +39,52 @@ public class LogicalOp {
             for (int i = x; i <= y; i++) {
                 System.out.println(i);
             }
-        }else {
-            for (int i = y; i <= x; i++){
+        } else {
+            for (int i = y; i <= x; i++) {
                 System.out.println(i);
             }
 
         }
     }
-    public void countOnlyPairNumbers(){
-        for (int i = 1; i <= 100; i++){
-            if (i % 2 == 0){
+
+    public void countOnlyPairNumbers() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
     }
-    public void countOnlyNonPairNumbers(){
-        for (int i = 1; i <= 100; i++){
-            if (i % 2 == 1){
+
+    public void countOnlyNonPairNumbers() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 1) {
                 System.out.println(i);
             }
         }
     }
+
     public void countSumOfNumbersFromTo(int r) {
         int sum = 0;
-        for (int i = r; i <= 100; i++){
+        for (int i = r; i <= 100; i++) {
             sum = sum + i;
         }
         System.out.println(sum);
     }
 
-    public void averageOfTheCountedSum(int r){
+    public void averageOfTheCountedSum(int r) {
         double sum = 0;
         double x = 100 - r;
-        for (int i = r; i <= 100; i++){
+        for (int i = r; i <= 100; i++) {
             sum = sum + i;
         }
         double average = sum / (x + 1);
         System.out.println(average);
     }
 
-    public void printingStars(){
+    public void printingStars() {
         String i = " ";
-        for (i = "*******"; i.length() > 0; i = i.substring(0, i.length()-1)){
-            if (i.length() % 4 == 0){
+        for (i = "*******"; i.length() > 0; i = i.substring(0, i.length() - 1)) {
+            if (i.length() % 4 == 0) {
                 System.out.println();
             }
             System.out.println(i);
